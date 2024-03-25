abc6e7bb69cc6ebc64070552de21f43f3d2b5838
sohee <sohee5143@naver.com>
2024-03-25 09:27:59 +0900

    ✨ Feat: 사진 저장 기능 추가

--- src/pages/Share.jsx
+++ src/pages/Share.jsx
@@ -5,51 +5,73 @@ import Button from "../components/common/Button";
 import HomeIcon from "../assets/images/HomeIcon.svg";
 import DownloadIcon from "../assets/images/DownloadIcon.svg";
 import InstaLogo from "../assets/images/InstaLogo.svg";
+import html2canvas from "html2canvas";
+import testImage4 from "../assets/images/testimage4.png";
+import { saveAs } from "file-saver";
 
 export default function Share() {
-  const DiaryImgList = [
-    "https://ifh.cc/g/4bZ6CR.png",
-    "https://ifh.cc/g/4bZ6CR.png",
-    "https://ifh.cc/g/4bZ6CR.png",
-    "https://ifh.cc/g/4bZ6CR.png",
-  ];
+  const DiaryImgList = [testImage4, testImage4, testImage4, testImage4];
   // 오늘 날짜
   var date = new window.Date();
 
   const Text =
     "오늘은 내 20살 생일이었다! 오전에 친구들과 클라이밍을 하고 생일파티를 했다-! 친구들이 생일 선물을 많이 줘서 기쁜 하루였다. 저녁에 아빠가 케이크를 사와서 가족들끼리 케이크도 먹었다! 행복한 생일~";
+
+  const onClickShareButton = () => {
+    console.log("onClcickShareButton");
+  };
+  const onClickDownloadButton = () => {
+    const target = document.getElementById("download");
+    if (!target) {
+      return alert("사진 저장에 실패했습니다.");
+    }
+    html2canvas(target, {
+      logging: true,
+      letterRendering: 1,
+      allowTaint: true,
+      useCORS: true,
+    }).then((canvas) => {
+      canvas.toBlob((blob) => {
+        if (blob !== null) {
+          saveAs(blob, "haru4cut.png");
+        }
+      });
+    });
+  };
   return (
     <MainWrap>
-      <LogoWrap>
-        <Logo>Haru4cut</Logo>
-        <LogoUnderLine />
-        <div>내 하루를 네컷으로 기록하세요</div>
-      </LogoWrap>
+      <DownWrap id="download">
+        <LogoWrap>
+          <Logo>Haru4cut</Logo>
+          <LogoUnderLine />
+          <div>내 하루를 네컷으로 기록하세요</div>
+        </LogoWrap>
 
-      <TodaysDiaryWrap>
-        <Date>{moment(date).format("YY.MM.DD")}</Date>
-        <Todays4CutDiary>오늘의 네컷일기</Todays4CutDiary>
-        <ImgWrap>
-          {DiaryImgList.map((imgUrl, index) => (
-            <>
-              <DiaryImage src={imgUrl} alt="하루네컷 이미지" />
-            </>
-          ))}
-        </ImgWrap>
-        <TextWrap>{Text}</TextWrap>
-      </TodaysDiaryWrap>
+        <TodaysDiaryWrap>
+          <Date>{moment(date).format("YY.MM.DD")}</Date>
+          <Todays4CutDiary>오늘의 네컷일기</Todays4CutDiary>
+          <ImgWrap>
+            {DiaryImgList.map((imgUrl, index) => (
+              <>
+                <DiaryImage src={imgUrl} alt="하루네컷 이미지" />
+              </>
+            ))}
+          </ImgWrap>
+          <TextWrap>{Text}</TextWrap>
+        </TodaysDiaryWrap>
+      </DownWrap>
       <div>오늘의 하루네컷을 완성했어요!</div>
       <DownloadButtonWrap>
-        <Button width="143px">
+        <Button width="143px" onClick={onClickShareButton}>
           <Icon src={InstaLogo} />
           인스타 공유
         </Button>
-        <Button width="143px">
+        <Button width="143px" onClick={onClickDownloadButton}>
           <Icon src={DownloadIcon} />
           앨범에 저장
         </Button>
       </DownloadButtonWrap>
-      <Button backgroundColor="#9D9D9D">
+      <Button backgroundColor="#9D9D9D" marginBottom="20px">
         <Icon src={HomeIcon} marginBottom="3px" />
         홈으로 돌아가기
       </Button>
@@ -64,6 +86,14 @@ const MainWrap = styled.div`
   height: 100vh;
   color: #8c8c8c;
   font-size: 14px;
+`;
+const DownWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
 `;
 const LogoWrap = styled.div`
   display: flex;
@@ -100,7 +130,7 @@ const TodaysDiaryWrap = styled.div`
   flex-direction: column;
   border-radius: 30px;
   width: 85%;
-  height: 70%;
+  height: 80%;
   margin-bottom: 20px;
   background-color: #a8c9e7;
 `;
