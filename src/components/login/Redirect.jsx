bf6bd72000ea7e67d84a2402436d697b809d937a
sohee <sohee5143@naver.com>
2024-05-29 17:14:39 +0900

    Feat: 캐릭터 생성 여부에 따라 연결 url 변경

--- src/components/login/Redirect.jsx
+++ src/components/login/Redirect.jsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsCharacterCreated } from "../../store";
 
 const Redirection = () => {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
-
+  const isCharacterCreated = useSelector((state) => state.isCharacterCreated);
   const fetchData = async () => {
     try {
       const response = await axios.post(
@@ -20,8 +22,12 @@ const Redirection = () => {
 
       console.log("로그인 성공", response.data);
 
-      // 새로 로그인한 경우 캐릭터 설정 페이지로 이동
-      navigate("/main");
+      // 캐릭터 생성 여부가 true이면 main으로, false이면 character로 이동
+      if (isCharacterCreated) {
+        navigate("/main");
+      } else {
+        navigate("/character");
+      }
     } catch (error) {
       console.error("로그인 오류 발생", error);
       // 오류 페이지나 로그인 페이지로 리디렉션 할 수 있습니다.
@@ -31,7 +37,7 @@ const Redirection = () => {
 
   useEffect(() => {
     fetchData();
-  }, [code]); // useEffect가 code에만 의존하도록 변경
+  }, [code, isCharacterCreated]); // useEffect가 code에만 의존하도록 변경
 
   // UI를 렌더링할 필요가 없는 경우가 많으므로 null을 반환
   return null;
