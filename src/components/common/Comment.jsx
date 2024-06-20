60ed59b40261ebc390c3850c445043963e61a647
sohee <sohee5143@naver.com>
2024-06-20 14:24:41 +0900

    feat: ai comment ui 추가

--- src/components/common/Comment.jsx
+++ src/components/common/Comment.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import styled from "styled-components";
 import CloseIcon from "../../assets/images/closeIcon.svg";
-
+import CommentIcon from "../../assets/images/commentIcon.svg";
 const Comment = ({ modalIsOpen, openModal, closeModal }) => {
   // 모달 스타일
   const customModalStyles = {
@@ -38,6 +38,12 @@ const Comment = ({ modalIsOpen, openModal, closeModal }) => {
         style={customModalStyles}
       >
         <CloseButton src={CloseIcon} alt="닫기" onClick={closeModal} />
+        <AIcommentWrap>
+          <img src={CommentIcon} alt="AI 코멘트" />
+          <AICommentText>
+            AI 친구의 일기 코멘트가 {"\n"}도착했습니다!
+          </AICommentText>
+        </AIcommentWrap>
       </Modal>
     </div>
   );
@@ -50,4 +56,20 @@ const CloseButton = styled.img`
   width: 27px;
   position: absolute;
   right: 22px;
+`;
+
+const AIcommentWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const AICommentText = styled.div`
+  color: #626793;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+  white-space: pre-wrap;
 `;
