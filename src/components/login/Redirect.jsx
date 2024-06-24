382a8b4fa5b91382bd893ec4361e02d7c97c1ac0
학생 정보통신공학 이수민 <yisumin317@office.inha.ac.kr>
2024-06-24 11:51:50 +0900

    redirect uri

--- src/components/login/Redirect.jsx
+++ src/components/login/Redirect.jsx
@@ -1,29 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useRef } from "react";
-import axios from "axios";
+import axiosInstance from "../../api/axiosInstance";
 import { useDispatch } from "react-redux";
 import { setUserId } from "../../store";
-import axiosInstance from "../../api/axiosInstance";
+
 const Redirection = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const hasFetchedData = useRef(false); // 추가된 부분
+  const hasFetchedData = useRef(false);
 
   const code = new URL(window.location.href).searchParams.get("code");
 
   const fetchData = async () => {
     try {
-      const response = await axiosInstance.post(
-        `/users/login/${code}`,
-        {},
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await axiosInstance.post(`/users/login/${code}`);
 
       console.log("로그인 성공", response.data);
+      navigate("/main");
       dispatch(setUserId(response.data.userId)); // userId를 Redux로 저장
 
       // 로컬스토리지에 token, userId 저장
@@ -33,11 +26,12 @@ const Redirection = () => {
       // 캐릭터 생성 여부를 판단하는 부분
       try {
         const characterResponse = await axiosInstance.get(
-          `/character/${localStorage.getItem("userId")}`
+          `/character/${response.data.userId}`
         );
-        console.log(characterResponse);
+        console.log("캐릭터 정보", characterResponse.data);
         navigate("/main");
       } catch (characterError) {
+        console.error("캐릭터 정보 조회 오류", characterError);
         navigate("/character");
       }
     } catch (error) {
@@ -48,13 +42,12 @@ const Redirection = () => {
 
   useEffect(() => {
     if (!hasFetchedData.current) {
-      // 추가된 부분
       fetchData();
-      hasFetchedData.current = true; // 추가된 부분
+      hasFetchedData.current = true;
     }
-  }, []);
+  }, []); // 빈 배열을 넣어 한 번만 실행되도록 설정
 
-  return null;
+  return <div>Redirecting...</div>;
 };
 
 export default Redirection;
