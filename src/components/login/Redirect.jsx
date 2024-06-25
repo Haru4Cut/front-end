72e71b7fd5e69a7fb0c0cb2ad4327190e5223dc1
sohee <sohee5143@naver.com>
2024-06-26 03:02:40 +0900

    fix: 404.html 설정

--- src/components/login/Redirect.jsx
+++ src/components/login/Redirect.jsx
@@ -25,7 +25,7 @@ const Redirection = () => {
         },
       };
       const response = await axiosInstance.post(
-        `http://52.79.154.88:8080/users/login/${code}`,
+        `/users/login/${code}`,
         {},
         {
           headers: {
